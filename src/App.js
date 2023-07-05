74f3e6412ffc5e01eae2cc139c642a21ddb79ce2
Joseph Davidson <josephdavidson@Josephs-MacBook-Pro.local>
2023-07-05 16:32:31 -0600

    logo added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='https://cdn.vox-cdn.com/thumbor/K-koxVfO7n7XtGkpEqKTu4ILDGE=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24130843/1433225105.jpg' className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
           SUP4
