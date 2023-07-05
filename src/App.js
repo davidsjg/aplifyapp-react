c31f7829be24ba06c793ab9da4ffa4fc6cb03c15
Joseph Davidson <josephdavidson@Josephs-MacBook-Pro.local>
2023-07-05 16:40:15 -0600

    fixed image

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='https://cdn.vox-cdn.com/thumbor/K-koxVfO7n7XtGkpEqKTu4ILDGE=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24130843/1433225105.jpg' className="App-logo" alt="logo" />
+        <img src='https://cdn.vox-cdn.com/thumbor/K-koxVfO7n7XtGkpEqKTu4ILDGE=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24130843/1433225105.jpg' alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          SUP4
+          Stanley Cup Champs 4ever
         </p>
         <a
           className="App-link"
